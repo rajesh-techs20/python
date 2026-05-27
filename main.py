00c435d6b511e69a3c857b9f8ffcb6b7c063c252
RAJESH G R <rgr98624@gmail.com>
2026-05-27 12:31:38 +0530

    Refactor student management system code

--- main.py
+++ main.py
@@ -40,3 +40,86 @@ while True:
         break
     else:
         print("Invalid Choice..Please Check once and Try again")
+
+OUTPUT:
+
+__Student Management System__
+
+1.Add Student
+2.Show Student Details
+3.Exit System
+Enter the choice:1
+Enter the number of students to add:10
+Enter name of student:Ram
+Enter the marks of a student:98
+
+__Student data saved succesfully__
+
+Enter name of student:Abhi 
+Enter the marks of a student:89
+
+__Student data saved succesfully__
+
+Enter name of student:Danu
+Enter the marks of a student:99
+
+__Student data saved succesfully__
+
+Enter name of student:Karthik
+Enter the marks of a student:78
+
+__Student data saved succesfully__
+
+Enter name of student:Sharath
+Enter the marks of a student:87
+
+__Student data saved succesfully__
+
+Enter name of student:Rohit
+Enter the marks of a student:85
+
+__Student data saved succesfully__
+
+Enter name of student:Hemanth
+Enter the marks of a student:84
+
+__Student data saved succesfully__
+
+Enter name of student:Mahesh
+Enter the marks of a student:55
+
+__Student data saved succesfully__
+
+Enter name of student:Kiran
+Enter the marks of a student:78
+
+__Student data saved succesfully__
+
+Enter name of student:Amith
+Enter the marks of a student:65
+
+__Student data saved succesfully__
+
+
+__Student Management System__
+
+1.Add Student
+2.Show Student Details
+3.Exit System
+Enter the choice:3
+EXITING THE SYSTEM.......
+PS C:\Users\Rajesh G R\Documents\oop project> 
+
+
+ADDED STUDENT MANAGEMENT SYSTEM FILE
+Ram 98
+Abhi 89
+Danu 99
+Karthik 78
+Sharath 87
+Rohit 85
+Hemanth 84
+Mahesh 55
+Kiran 78
+Amith 65
+
