08fe8718920d47d7789ac8863e1c1ec7fd0d9dce
RAJESH G R <rgr98624@gmail.com>
2026-06-07 13:11:17 +0530

    Implement containsDuplicate method in Solution class

---
+++ containsDuplicate.py
@@ -0,0 +1,10 @@
+#217 Leetcode
+
+class Solution(object):
+  def containsDuplicate(self,nums):
+    dup=set()
+    for num in nums:
+      if num in dup:
+        return True
+      dup.add(num)
+    return False
