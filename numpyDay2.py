9c4958250fbcaa3b2c411afbb1e088e309a36560
RAJESH G R <rgr98624@gmail.com>
2026-06-18 23:10:39 +0530

    Implement array indexing examples in numpyDay2.py
    Added Task 1 to demonstrate array indexing in NumPy.

--- numpyDay2.py
+++ numpyDay2.py
@@ -15,3 +15,18 @@ print(arr[3:]) #last two elements
 [20 30 40]
 [40 50]
 
+
+#TASK1
+c=np.array([5,10,15,20,25,30])
+print(c[0]) #first element
+print(c[-1]) #last element
+print(c[1:5]) #index 1 to 4
+print(c[:4]) #first four elements
+print(c[3:])  #last 3 elements
+
+#OUTPUT
+5
+30
+[10 15 20 25]
+[ 5 10 15 20]
+[15 20 25 30]
