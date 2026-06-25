c77bec70fea7045ecc227257f07b9ef681564945
RAJESH G R <rgr98624@gmail.com>
2026-06-25 22:54:24 +0530

    Add histogram chart for marks distribution

--- piehist.py
+++ piehist.py
@@ -6,3 +6,14 @@ marks=[89,90,97,78,80]
 plt.pie(marks,labels=subjects,autopct="%1.1f%%")
 plt.title("Marks Distribution")
 plt.show()
+
+
+#HISTAGRAM CHART
+
+import matplotlib.pyplot as plt
+marks=[90,80,70,60,50,40]
+plt.hist(marks)
+plt.xlabel("Marks")
+plt.ylabel("Number of Students")
+plt.title("Histagram of marks")
+plt.show()
