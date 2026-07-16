8225f25512bfee3d55ff73f4c85237363870bae6
RAJESH G R <rgr98624@gmail.com>
2026-07-16 20:42:39 +0530

    Update sk.py

--- sk.py
+++ sk.py
@@ -126,3 +126,22 @@ model.fit(X,y)
 
 prediction=model.predict(pd.DataFrame({"Marks":[2.4]}))
 print(prediction)
+
+NAIVE BAYES THEOREM
+from sklearn.naive_bayes import GaussianNB
+
+data={
+    "Marks":[1,2,3,4,5],
+    "Result":["Fail","Fail","Pass","Pass","Pass"]
+}
+
+df=pd.DataFrame(data)
+X=df[["Marks"]]
+y=df["Result"]
+
+model=GaussianNB()
+model.fit(X,y)
+
+prediction=model.predict(pd.DataFrame({"Marks":[2.4]}))
+print(prediction)
+
